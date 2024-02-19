aa85ff97689a6314641191110071066a8bdceac1
PT013-Sanjayan-Backend Developer <piccotvm13@gmail.com>
2024-02-19 18:26:41 +0530

    Build file working

--- main.go
+++ main.go
@@ -98,7 +98,7 @@ func getUserInputs() {
 	c := exec.Command("go", "env", "-w", "GO111MODULE=off")
 	c.Run()
 	// Define the Git repository URL and the directory where you want to clone it
-	repoURL := "git@github.com:spurtcms/adminpanel.git"
+	repoURL := "https://github.com/spurtcms/spurtcms-admin.git"
 	// If it doesn't exist, clone the repository
 	cmd11 := exec.Command("git", "clone", "-b", "main", repoURL)
 
@@ -114,7 +114,7 @@ func getUserInputs() {
 	mydir, _ := os.Getwd()
 	// fmt.Println("mydir", mydir)
 	// Change the working directory of the current process
-	err1 := os.Chdir(mydir + "/adminpanel")
+	err1 := os.Chdir(mydir + "/spurtcms-admin")
 	if err1 != nil {
 		fmt.Println(Red, "Error changing directory:", Reset, err1)
 		return
@@ -135,18 +135,18 @@ func getUserInputs() {
 	// fmt.Print("Enter the PostgreSQL DB_HOST : ")
 	// command1, _ := reader.ReadString('\n')
 	// command1 = command1[:len(command1)-1]
-	var host, dbName, userName, password string
+	var host, port, dbName, userName, password string
 
 	// Open a connection to the database.
 	for {
-		host, dbName, userName, password = GetDbCredential()
+		host, port, dbName, userName, password = GetDbCredential()
 		fmt.Println("dbname", dbName)
 		// fmt.Println("PostgreSQL database credential changed successfully!")
 		// fmt.Println("connection", userName, password, host)
 		// Connection string to connect to the PostgreSQL database.
 		// Construct the connection string.
 		connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=disable",
-			host, "5432", userName, password)
+			host, port, userName, password)
 		// connStr := "user=" + userName + " password=" + password + " host=" + host + " port=5432 sslmode=disable"
 		// fmt.Println("connection String : ", connectionString)
 		db, err := sql.Open("postgres", connectionString)
@@ -164,12 +164,19 @@ func getUserInputs() {
 			continue
 		}
 		fmt.Println(Green, "Connected to the database.", Reset)
-		// Create the database
-		_, err = db.Exec("CREATE DATABASE " + dbName)
-		if err != nil {
-			log.Fatal("Error creating database: ", err)
+
+		// Check if the database exists
+		if databaseExists(db, dbName) {
+			fmt.Println(Red, "Database already exists.", Reset)
+			continue
+		} else {
+			// Create the database
+			_, err = db.Exec("CREATE DATABASE " + dbName)
+			if err != nil {
+				log.Fatal("Error creating database: ", err)
+			}
+			fmt.Println(Green, "Database created successfully!", Reset)
 		}
-		fmt.Println(Green, "Database created successfully!", Reset)
 
 		// Define the path to your SQL backup file
 		backupFile := mydir + "/spurtcm.sql"
@@ -191,21 +198,48 @@ func getUserInputs() {
 	cmd1 := exec.Command("go", "env", "-w", "GO111MODULE=on")
 	cmd1.Run()
 
-	cmd33 := exec.Command("go", "mod", "tidy")
-	err2 := cmd33.Run()
-	if err2 != nil {
-		fmt.Println("Error go mod tidy :", err2)
-		return
-	}
-	cmd55 := exec.Command("go", "mod", "vendor")
-	cmd55.Run()
-	fmt.Println("Listening and serving HTTP on : " + os.Getenv("DOMAIN_URL"))
-	cmd44 := exec.Command("go", "run", "main.go")
+	// cmd33 := exec.Command("go", "mod", "tidy")
+	// err2 := cmd33.Run()
+	// if err2 != nil {
+	// 	fmt.Println("Error go mod tidy :", err2)
+	// 	return
+	// }
+	// cmd55 := exec.Command("go", "mod", "vendor")
+	// cmd55.Run()
+
+	cmd66 := exec.Command("chmod", "-R", "777", "admin-panel")
+	cmd66.Run()
+	mydir1, _ := os.Getwd()
+	fmt.Println("mydir1", mydir1)
+	// Set environment variables
+
+	// Change the working directory of the current process
+	// errr1 := os.Chdir(mydir1)
+	// if errr1 != nil {
+	// 	fmt.Println(Red, "Error changing directory:", Reset, errr1)
+	// 	return
+	// }
+	// Load the .env file
+	// err111 := godotenv.Load(".env")
+	// if err111 != nil {
+	// 	fmt.Println(Red, "Error loading .env file:", Reset, err111)
+	// 	return
+	// }
+	os.Setenv("DB_HOST", host)
+	os.Setenv("DB_PORT", port)
+	os.Setenv("DB_USERNAME", userName)
+	os.Setenv("DB_PASSWORD", password)
+	os.Setenv("DB_NAME", dbName)
+	cmd44 := exec.Command("./admin-panel")
+	cmd44.Stdout = os.Stdout
+	cmd44.Stderr = os.Stderr
 	err3 := cmd44.Run()
 	if err3 != nil {
-		fmt.Println("Error on main function :", err3)
+		fmt.Println(Red, "Error on main function :", Reset, err3)
 		return
 	}
+
+	fmt.Println("Listening and serving HTTP on : " + os.Getenv("DOMAIN_URL"))
 	fmt.Println("Main file run!")
 	// Continue with more commands as needed
 }
@@ -258,51 +292,47 @@ func ChangeEnvContent(key, val string) {
 	writer.Flush()
 }
 
-func GetDbCredential() (host, dbName, userName, password string) {
+func GetDbCredential() (host, port, dbName, userName, password string) {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 
 		fmt.Print("Enter the PostgreSQL DB_HOST : ")
-		command1, _ := reader.ReadString('\n')
-		command1 = command1[:len(command1)-1]
-		host = command1
+		command, _ := reader.ReadString('\n')
+		command = command[:len(command)-1]
+		host = command
 		// fmt.Println("length", len(command1))
 		// Split the input into command and arguments
 		// parts := strings.Fields(command1)
-		if len(command1) == 0 && command1 == "" {
+		if len(command) == 0 && command == "" {
 
 			fmt.Println("Please Enter the postgres DB_HOST credential...!")
 			continue
 		}
 
-		if len(command1) != 0 {
-			ChangeEnvContent("DB_HOST", command1)
+		if len(command) != 0 {
+			ChangeEnvContent("DB_HOST", command)
 			break
 		}
 
 	}
 
-	// Prompt user for the second command
-	// fmt.Print("Enter the PostgreSQL DB_NAME: ")
-	// command2, _ := reader.ReadString('\n')
-	// command2 = command2[:len(command2)-1]
 	for {
 
-		fmt.Print("Enter the PostgreSQL DB_NAME : ")
-		command2, _ := reader.ReadString('\n')
-		command2 = command2[:len(command2)-1]
-		dbName = command2
-		// fmt.Println("length", len(command2))
+		fmt.Print("Enter the PostgreSQL DB_PORT : ")
+		command1, _ := reader.ReadString('\n')
+		command1 = command1[:len(command1)-1]
+		port = command1
+		// fmt.Println("length", len(command1))
 		// Split the input into command and arguments
 		// parts := strings.Fields(command1)
-		if len(command2) == 0 && command2 == "" {
+		if len(command1) == 0 && command1 == "" {
 
-			fmt.Println("Please Enter the postgres DB_NAME credential...!")
+			fmt.Println("Please Enter the postgres DB_PORT credential...!")
 			continue
 		}
 
-		if len(command2) != 0 {
-			ChangeEnvContent("DB_NAME", command2)
+		if len(command1) != 0 {
+			ChangeEnvContent("DB_PORT", command1)
 			break
 		}
 
@@ -359,5 +389,42 @@ func GetDbCredential() (host, dbName, userName, password string) {
 		}
 
 	}
-	return host, dbName, userName, password
+
+	// Prompt user for the second command
+	// fmt.Print("Enter the PostgreSQL DB_NAME: ")
+	// command2, _ := reader.ReadString('\n')
+	// command2 = command2[:len(command2)-1]
+	for {
+
+		fmt.Print("Enter the PostgreSQL DB_NAME : ")
+		command2, _ := reader.ReadString('\n')
+		command2 = command2[:len(command2)-1]
+		dbName = command2
+		// fmt.Println("length", len(command2))
+		// Split the input into command and arguments
+		// parts := strings.Fields(command1)
+		if len(command2) == 0 && command2 == "" {
+
+			fmt.Println("Please Enter the postgres DB_NAME credential...!")
+			continue
+		}
+
+		if len(command2) != 0 {
+			ChangeEnvContent("DB_NAME", command2)
+			break
+		}
+
+	}
+	return host, port, dbName, userName, password
+}
+
+// databaseExists checks if the specified database exists
+func databaseExists(db *sql.DB, dbName string) bool {
+	query := "SELECT EXISTS(SELECT 1 FROM pg_database WHERE datname=$1)"
+	var exists bool
+	err := db.QueryRow(query, dbName).Scan(&exists)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return exists
 }
